59dff019ae71ad50a5fa4be8fee1fdb7fd646ba8
Sangjun Park <sangjun1997@naver.com>
2022-03-04 16:05:52 +0900

    + annotation

--- src/app/base-graph/base-graph.component.ts
+++ src/app/base-graph/base-graph.component.ts
@@ -3,7 +3,6 @@ import { MultiGraph } from 'graphology';
 import * as d3 from 'd3';
 import louvain from 'graphology-communities-louvain';
 
-
 @Component({
   selector: 'app-base-graph',
   templateUrl: './base-graph.component.html',
@@ -16,12 +15,9 @@ export class BaseGraphComponent implements OnInit, AfterViewInit {
   id: number = 0;
   x: number = 0;
   y: number = 0;
-  clusterNum : number = 0;
+  clusterNum : number = 0; // used in input box
 
-  constructor() { 
-
-  }
-
+  constructor() {}
   ngOnInit(): void {
   }
 
@@ -35,10 +31,9 @@ export class BaseGraphComponent implements OnInit, AfterViewInit {
       });
   }
 
-
   render(bus: any, branch: any): void {
 
-    const graph = new MultiGraph();
+    const graph = new MultiGraph(); // duplicated edges -> Multi Graph
 
     const size = {
       viewBox: { minX: 0, minY: 0, width: 1000, height: 1000 },
@@ -47,18 +42,15 @@ export class BaseGraphComponent implements OnInit, AfterViewInit {
     };
 
     console.log("bus property", bus.x, bus.y)
-    const x = Object.keys(bus.x).map((d): any => { // x좌표
+    const x = Object.keys(bus.x).map((d): any => { // x
       return bus.x[d];
     });
-    const y = Object.keys(bus.y).map((d): any => { // y좌표
+    const y = Object.keys(bus.y).map((d): any => { // y
       return bus.y[d];
     });
-    const xY = Object.keys(bus.x).map((d): any => {
+    const xY = Object.keys(bus.x).map((d): any => { // Object mapping
       return { id: bus["bus id"][d], x: bus.x[d], y: bus.y[d] };
     });
-
-    console.log(x);
-    console.log(bus.x);
 
     for(let i=0; i<xY.length; i++){
       graph.addNode(xY[i].id);
@@ -67,27 +59,18 @@ export class BaseGraphComponent implements OnInit, AfterViewInit {
     for (let i = 0; i < branch.length; i++) {
       graph.addEdge(branch[i].from, branch[i].to); // 중복 있어서 multi graph로 만듦
     }
-    console.log(graph);
-    console.log(graph.nodes());
-    // console.log(graph.edges());
 
-    const communities = louvain(graph);
-    console.log(communities);
-
-    // console.log(communities[67]); // 0 : cluster num
-    // console.log(communities[68]); // 1 : cluster num
-    // console.log(communities[1062]); // 18 : cluster num
+    const communities = louvain(graph); // assign Louvain Algorithm
+    console.log(communities); // data type : number[]
 
     for(let i=0; i<19; i++){
-      this.nodeGroups[i] = i;
+      this.nodeGroups[i] = i; // [0, 1, ... , 18]
     }
 
     const a = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c',
      '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075'];
     const color = d3.scaleOrdinal(this.nodeGroups, a);
-   
-// Louvain algorithm 적용 : 클러스터링 인덱스 부여
-
+// Louvain algorithm -> attach colors
 
     const xMin = 0;
     const yMin = 0;
@@ -106,49 +89,46 @@ export class BaseGraphComponent implements OnInit, AfterViewInit {
     const xScale = d3.scaleLinear(xDomain, xRange);
     const yScale = d3.scaleLinear(yDomain, yRange);
 
-
-    const colorLinkedNodes_from = (d: any, linkedNodes: number[]) => { // linkedNodes_from.push() 하는 함수 따로, return하는 함수 따로 -> // for문 내에서 return하는 함수 반복 호출
+    const colorLinkedNodes_from = (d: any, linkedNodes: number[]) => { // for linkedNodes_from.push()
       edges.filter((s: any, j: any) => {
         return +s.from === +d.id;
       }).filter((h: any, k: any) => {
         linkedNodes.push(+h.to);
         return h.to;
       });
-    } // 함수 따로 빼서 호출하는 방식으로 여러 리턴값 처리
+    }
 
-    const colorLinkedNodes_to = (d: any, linkedNodes: number[]) => { // linkedNodes_to.push() 하는 함수 따로, return하는 함수 따로 -> // for문 내에서 return하는 함수 반복 호출
+    const colorLinkedNodes_to = (d: any, linkedNodes: number[]) => { // for linkedNodes_to.push() 
       edges.filter((s: any, j: any) => {
         return +s.to === +d.id;
       }).filter((h: any, k: any) => {
         linkedNodes.push(+h.from);
         return h.from;
       });
-    } // 함수 따로 빼서 호출하는 방식으로 여러 리턴값 처리
-
+    }
 
     const mouseover = (event: any, d: any) => {
       console.log('id : ' + d.id);
       this.id = d.id;
       this.x = d.x;
       this.y = d.y;
-      this.clusterNum = communities[d.id];
+      this.clusterNum = communities[d.id]; // for input box in html file.
 
-      // 선택 노드
+      // selected node
       nodes.filter((m, i) => {
         return m === d;
       })
-        // .attr("fill", d => color(d.id % 19))
         .attr('fill', 'blue')
         .attr("fill-opacity", 1);
 
-      // 미선택 노드
+      // other nodes
       nodes.filter((m, i) => {
         return m !== d;
       })
         .attr("fill", d => color(d.id % 19))
         .attr("fill-opacity", 0.1);
 
-      // d.id (마우스오버된 노드의 id) 가, s.from 과 같은 값을 가질 때, 그 때의 s.to를 id로 갖는 노드를 색칠
+      // Highlight 'red' nodes : starts from selected node(mouse-overed node).
       let linkedNodes_from: number[] = [];
       let countNum = 0;
       colorLinkedNodes_from(d, linkedNodes_from);
@@ -160,8 +140,7 @@ export class BaseGraphComponent implements OnInit, AfterViewInit {
           .attr("fill-opacity", 1);
       }
 
-
-      // d.id (마우스오버된 노드의 id) 가, s.to 과 같은 값을 가질 때, 그 때의 s.from을 id로 갖는 노드를 색칠
+      // Highlight 'green' nodes : ends at selected node.
       let linkedNodes_to: number[] = [];
       countNum = 0;
       colorLinkedNodes_to(d, linkedNodes_to);
@@ -172,11 +151,9 @@ export class BaseGraphComponent implements OnInit, AfterViewInit {
           .attr('fill', 'green')
           .attr("fill-opacity", 1);
       }
-      
 
-
-      
-      // 나가는 edges(from) : red
+      // edges(from) : red.
+      // starts from selected node.
       edges.filter((m: any, i) => {
         return m.from == d.id;
       })
@@ -184,7 +161,8 @@ export class BaseGraphComponent implements OnInit, AfterViewInit {
         .attr("stroke-width", "2px")
         .attr("stroke-opacity", 1);
       
-      // 들어오는 edges(to) : green
+      // edges(to) : green.
+      // ends at selected node.
       edges.filter((m: any, i) => {
         return m.to == d.id;
       })  
@@ -192,49 +170,44 @@ export class BaseGraphComponent implements OnInit, AfterViewInit {
         .attr("stroke-width", "2px")
         .attr("stroke-opacity", 1);
 
-      // 그 외 edges
+      // other edges (no relevance).
       edges.filter((m: any, i) => {
         return (m.from != d.id && m.to != d.id); // m == nothing
       })
         .attr("stroke-width", "1px")
         .attr("stroke-opacity", 0.05);
-      
-      
     };
 
-    const mouseout = (event: any, d: any) => {
-      // console.log("mouseout event", event, d);
+    const mouseout = (event: any, d: any) => { // same with first state.
       nodes.attr("fill", d => color(communities[d.id]))
         .attr("fill-opacity", 1);
 
       edges.attr("stroke-width", "1px")
         .attr("stroke", "steelblue")
         .attr("stroke-opacity", 0.2);
-
     }
 
     const svg = d3.select("#base-graph")
       .attr("viewBox", `${size.viewBox.minX}, ${size.viewBox.minY}, ${size.viewBox.width}, ${size.viewBox.height}`);
 
-
     function drawEdge(d: any): any {
 
-      let k = `M${xScale(x[d.from-1])}, ${yScale(y[d.from-1])}`; // path 시작 지점
-      let xdif = x[d.to-1] - x[d.from-1]; // x좌표 차이
-      let ydif = y[d.to-1] - y[d.from-1]; // y좌표 차이
-      let abs_xdif = Math.abs(xdif); // |x좌표 차이| 
-      let abs_ydif = Math.abs(ydif); // |y좌표 차이|
+      let k = `M${xScale(x[d.from-1])}, ${yScale(y[d.from-1])}`; // 'path' starting point
+      let xdif = x[d.to-1] - x[d.from-1]; // x diff
+      let ydif = y[d.to-1] - y[d.from-1]; // y diff
+      let abs_xdif = Math.abs(xdif); // |x diff|
+      let abs_ydif = Math.abs(ydif); // |y diff|
 
-      let xhalf = xScale((x[d.to-1] + x[d.from-1]) /2);
-      let yhalf = yScale((y[d.to-1] + y[d.from-1]) /2);
+      let xhalf = xScale((x[d.to-1] + x[d.from-1]) /2); // x's half point between source & target.
+      let yhalf = yScale((y[d.to-1] + y[d.from-1]) /2); // y's half point between source & target.
 
-      if(abs_xdif > abs_ydif) {
-        k += `L${xScale(x[d.from-1])}, ${yhalf}`;
+      if(abs_xdif > abs_ydif) { // if |x diff| > |y diff|
+        k += `L${xScale(x[d.from-1])}, ${yhalf}`; // starts drawing : Vertical.
         k += `L${xScale(x[d.to - 1])}, ${yhalf}`;
         k += `L${xScale(x[d.to - 1])}, ${yScale(y[d.to - 1])}`;
       }
-      else {
-        k += `L${xhalf}, ${yScale(y[d.from-1])}`;
+      else { // if |x diff| <= |y diff|
+        k += `L${xhalf}, ${yScale(y[d.from-1])}`; // starts drawing : Horizontal.
         k += `L${xhalf}, ${yScale(y[d.to - 1])}`;
         k += `L${xScale(x[d.to - 1])}, ${yScale(y[d.to - 1])}`; 
       }
@@ -250,7 +223,6 @@ export class BaseGraphComponent implements OnInit, AfterViewInit {
       .attr("fill", "none")
       .attr("stroke-opacity", 0.2)
 
-
     const nodes = svg.append("g")
       .selectAll("rect")
       .data(xY)
@@ -264,15 +236,13 @@ export class BaseGraphComponent implements OnInit, AfterViewInit {
       .on("mouseover", mouseover)
       .on("mouseout", mouseout);
 
-    nodes
+    nodes // add color based on cluster number.
     .attr('fill', d => color(d.id));
 
-    nodes // add tooltip
+    nodes // add tooltip.
     .append('title') 
     .text((d:any) => (`id : ${d.id}\n` + `x : ${d.x}\n` + `y : ${d.y}\n` + `cluster num : ${communities[d.id]}`))
     .style("top", (d: any) => d.y)
     .style("left", (d: any) => d.x);
-
   }
-
 }
