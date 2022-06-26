b824595a4c1dc2e7dce9bda4f02ca1a852275956
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2022-06-26 18:10:47 +0900

    base-graph remove

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BaseGraphComponent } from './base-graph/base-graph.component';
 import { WorkspaceComponent } from './workspace/workspace.component';
 import { TreemapComponent } from './treemap/treemap.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BaseGraphComponent,
     WorkspaceComponent,
     TreemapComponent
   ],
