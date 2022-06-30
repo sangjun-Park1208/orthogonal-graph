238c2b0c60826573ec2ead727fd6e9114c3480f5
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2022-06-30 17:16:41 +0900

    random_data

--- src/app/treemap/treemap.component.ts
+++ src/app/treemap/treemap.component.ts
@@ -25,10 +25,10 @@ export class TreemapComponent implements OnInit {
   }
 
   ngAfterViewInit(): void {
-    d3.csv('./assets/data/bus-14.csv')
+    d3.csv('./assets/data/bus-1062.csv')
     // d3.csv('./assets/data/my_bus.csv')
       .then((bus: any) => {
-        d3.csv('./assets/data/branch-14.csv')
+        d3.csv('./assets/data/branch-1062.csv')
         // d3.csv('./assets/data/my_branch.csv')
           .then((branch: any) => {
             console.log("bus, branch", bus, branch);
