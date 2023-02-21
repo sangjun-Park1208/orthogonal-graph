efe407cd8c1a0d88571b278cf2aabfbe6bf48202
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2023-02-21 23:28:12 +0900

    to LAB

--- src/app/treemap/treemap.component.ts
+++ src/app/treemap/treemap.component.ts
@@ -4,16 +4,8 @@ import * as d3 from "d3"
 import louvain from 'graphology-communities-louvain';
 import { IBranchData } from 'src/shared/interfaces/ibranch-data';
 import { IBusData } from 'src/shared/interfaces/ibus-data';
-import { TreemapData } from 'src/shared/modules/treemap-vis/datas/treemap-data';
-import { seqeunce_TreemapData } from 'src/shared/modules/treemap-vis/datas/seqeunce-treemap-data';
-import { local_Random_TreemapData } from 'src/shared/modules/treemap-vis/datas/local-random-treemap-data';
-import { global_Random_TreemapData } from 'src/shared/modules/treemap-vis/datas/global-random-treemap-data';
-import { TreemapSelections } from 'src/shared/modules/treemap-vis/selections/treemap-selections';
-import { TreemapSelectionsDevided } from 'src/shared/modules/treemap-vis/selections/treemap-selections-devided';
-import { TreemapEventListeners } from 'src/shared/modules/treemap-vis/event-listeners/treemap-event-listeners';
-import { EdgeCrossingCountCalculator } from 'src/shared/modules/treemap-vis/calculate edge crossing/calculate-edge-crossing';
-import { EdgeMeasurement } from 'src/shared/modules/treemap-vis/calculate edge crossing/edge-measurement';
-import { MatButtonToggleGroup } from '@angular/material/button-toggle';
+import { RandomStatisticsService } from 'src/shared/modules/treemap-vis/datas/random-statistics.service';
+import { IStatisticData } from 'src/shared/interfaces/istatistic.data';
 
 declare var require: any //jStat 쓰기 위해 추가
 
@@ -32,40 +24,35 @@ export class TreemapComponent implements OnInit {
   port: number[];
   measurement: number[]
   mesu_name: String[];
-  toggle: String;
-  togglenum: number;
   portnum: number;
   // random_measurement: number[]
   statistics_toggle: String;
-  random_mean: number[];
-  random_median: number[];
-  random_min: number[];
-  random_max: number[];
-  random_std: number[];
   statistics_index: number;
-  iter: string;
   bind_time: string;
+  iter: string;
+  toggle: string;
+  togglenum: number;
+  statisticdata:IStatisticData;
+  
 
   ngOnInit(): void {
   }
 
-  constructor() {
+  constructor(private rs:RandomStatisticsService) {
     this.data = [14, 30, 57, 118, 300, 1062];
     this.measurement = [];
     this.port=[4,12];
     this.mesu_name = ["Total Length", "Edge Crossing", "Total Bending"];
-    this.toggle = "Z_Layout";
-    this.togglenum = 1062;
+    this.toggle="Z_Layout";
+    this.togglenum=1062;
+    this.rs.togglenum.next(1062);
     this.portnum=12;
     // this.random_measurement = [0, 0, 0];
+    this.statisticdata=rs.statisticdata;
     this.statistics_toggle="Total_Length";
     this.statistics_index=0;
-    this.random_mean=[0,0,0];
-    this.random_median=[0,0,0];
-    this.random_min=[0,0,0];
-    this.random_max=[0,0,0];
-    this.random_std=[0,0,0];
-    this.iter='1000';
+    this.iter='1000'
+    this.rs.iter.next(this.iter);
     this.bind_time=`0`;
   }
 
@@ -85,8 +72,9 @@ export class TreemapComponent implements OnInit {
   }
 
   select(num: number) {
-    console.log(num)
-    this.togglenum=num;
+    console.log('iter : ',this.iter);
+    this.rs.iter.next(this.iter);
+    this.rs.togglenum.next(num);
     d3.csv(`./assets/data/bus-${num}.csv`)
       .then((bus: any) => {
         d3.csv(`./assets/data/branch-${num}.csv`)
@@ -98,15 +86,15 @@ export class TreemapComponent implements OnInit {
             this.bind_time=t.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
           })
       });
-
   }
+
 
   port_select(num: number){
     this.portnum=num;
     console.log(`port`,this.portnum);
-    d3.csv(`./assets/data/bus-${this.togglenum}.csv`)
+    d3.csv(`./assets/data/bus-${this.rs.togglenum$}.csv`)
       .then((bus: any) => {
-        d3.csv(`./assets/data/branch-${this.togglenum}.csv`)
+        d3.csv(`./assets/data/branch-${this.rs.togglenum$}.csv`)
           .then((branch: any) => {
             let start= new Date().getTime();
             this.renderTreemap(bus, branch);
@@ -118,10 +106,12 @@ export class TreemapComponent implements OnInit {
   }
 
   ToggleSelect(event: any) {
-    this.toggle = event;
-    d3.csv(`./assets/data/bus-${this.togglenum}.csv`)
+    console.log('iter : ',this.iter);
+    this.rs.iter.next(this.iter);
+    this.rs.toggle.next(event);
+    d3.csv(`./assets/data/bus-${this.rs.togglenum$}.csv`)
       .then((bus: any) => {
-        d3.csv(`./assets/data/branch-${this.togglenum}.csv`)
+        d3.csv(`./assets/data/branch-${this.rs.togglenum$}.csv`)
           .then((branch: any) => {
             let start= new Date().getTime();
             this.renderTreemap(bus, branch);
@@ -134,7 +124,6 @@ export class TreemapComponent implements OnInit {
 
   StatisticsSelect(event: any){
     this.statistics_toggle=event;
-    console.log('chan', this.statistics_toggle);
     if(this.statistics_toggle=="Total_Length"){
       this.statistics_index=0;
     }
@@ -155,6 +144,8 @@ export class TreemapComponent implements OnInit {
   // }
 
   renderTreemap(bus: IBusData[], branch: IBranchData[]): void {
+    this.rs.bus=bus;
+    this.rs.branch=branch;
     console.log({ bus, branch })
     const size = {
       width: 1700,
@@ -173,10 +164,22 @@ export class TreemapComponent implements OnInit {
       cluster: 2,
       edge: 1.5
     };
+    
+    this.rs.togglenum$.subscribe((d)=>{
+      this.togglenum=d;
+    });
     const nodeSize = 371.13274*Math.pow(this.togglenum,-0.42682);
     console.log('nodeSize',nodeSize);
     const graph = new MultiGraph(); // duplicated edges -> Multi Graph
+    this.rs.size=size;
+    this.rs.opacity=opacity;
+    this.rs.strokeWidth=strokeWidth;
+    this.rs.nodeSize=nodeSize;
 
+    this.rs.iter$.subscribe((d)=>{
+      this.iter=d;
+    });
+    
     // 상준형 graphology 코드
     for (let i = 0; i < bus.length; i++) {
       graph.addNode(bus[i].id);
@@ -187,8 +190,10 @@ export class TreemapComponent implements OnInit {
     }
 
     const details = louvain.detailed(graph, { randomWalk: false, resolution: 0.1 }); // assign Louvain Algorithm
-
+    this.rs.details=details;
     console.log("details", details);
+
+    
 
     const svg = d3.select(this.rootSvg.nativeElement)
       .attr("viewBox", `${-size.viewBox.minX} ${-size.viewBox.minY} ${size.viewBox.width + size.margin.right} ${size.viewBox.height + size.margin.bottom}`)
@@ -196,116 +201,59 @@ export class TreemapComponent implements OnInit {
       .attr("height", size.height)
       .on("click", (event, d) => {
         // console.log("svg click", event, d);
-        treemapEventListeners.restoreViewBox(event, d);
+        this.rs.treemapEventListeners.restoreViewBox(event, d);
       });
     // 다시 그리기
     svg.select('g.container').remove();
     // 그룹
     const root = svg.append("g")
       .attr("class", "container")
-    let treemapData!: TreemapData | seqeunce_TreemapData | local_Random_TreemapData | global_Random_TreemapData;
 
-    let edgeMeasurement: EdgeMeasurement;
-    // let treemapSelections: TreemapSelections;
-    let treemapSelections: TreemapSelections|TreemapSelectionsDevided; //chan adding
-    let treemapEventListeners: TreemapEventListeners;
-    if(this.iter=='') this.iter='1';
-    let random_count = Number(this.iter);
-    console.log('chaskdhfkladsjf',random_count);
+    //random-statistics.services.ts
+    this.rs.root=root;
 
-    var {jStat}=require('jstat');
-    let l_stat=new Array();//total length stat
-    let e_stat=new Array();//edge crossing stat
-    let b_stat=new Array();//total bending stat
+    this.rs.toggle.next(this.toggle);
+    // this.measurement=this.rs.measurement;
+    // this.statisticdata=this.rs.statisticdata;
+    this.rs.measurement$.subscribe((d:any)=>{
+      console.log('chan',d);
+      this.measurement=d;
+    });
+    this.rs.statisticdata$.subscribe((d:any)=>{
+      console.log('chan',d);
+      this.statisticdata=d;
+    });
 
-    if (this.toggle == "Z_Layout")
-      treemapData = new TreemapData(bus, branch, details, size, nodeSize, strokeWidth, opacity);
-    else if (this.toggle == "Sequence")
-      treemapData = new seqeunce_TreemapData(bus, branch, details, size, nodeSize, strokeWidth, opacity);
-    else if (this.toggle == "Local_Random") {
-      for (let i = 0; i < random_count; i++) {
-        treemapData = new local_Random_TreemapData(bus, branch, details, size, nodeSize, strokeWidth, opacity);
-        treemapData.setZNodePosition();
-        edgeMeasurement = new EdgeMeasurement(treemapData, branch);
-        l_stat.push(edgeMeasurement.calculateEdgeCrossingCount()[0]);
-        e_stat.push(edgeMeasurement.calculateEdgeCrossingCount()[1]);
-        b_stat.push(edgeMeasurement.calculateEdgeCrossingCount()[2]);
-      }
-    }
-    else if (this.toggle == "Global_Random") {
-      for (let i = 0; i < random_count; i++) {
-        treemapData = new global_Random_TreemapData(bus, branch, details, size, nodeSize, strokeWidth, opacity);
-        treemapData.setZNodePosition();
-        edgeMeasurement = new EdgeMeasurement(treemapData, branch);
-        l_stat.push(edgeMeasurement.calculateEdgeCrossingCount()[0]);
-        e_stat.push(edgeMeasurement.calculateEdgeCrossingCount()[1]);
-        b_stat.push(edgeMeasurement.calculateEdgeCrossingCount()[2]);
-      }
-    }
-    treemapData.setZNodePosition();
-    
-    if (this.toggle == "Local_Random" || this.toggle == "Global_Random") {
-      this.random_mean[0] = jStat(l_stat).mean();
-      this.random_mean[1] = jStat(e_stat).mean();
-      this.random_mean[2] = jStat(b_stat).mean();
-      this.random_median[0] = jStat(l_stat).median();
-      this.random_median[1] = jStat(e_stat).median();
-      this.random_median[2] = jStat(b_stat).median();
-      this.random_min[0] = jStat(l_stat).min();
-      this.random_min[1] = jStat(e_stat).min();
-      this.random_min[2] = jStat(b_stat).min();
-      this.random_max[0] = jStat(l_stat).max();
-      this.random_max[1] = jStat(e_stat).max();
-      this.random_max[2] = jStat(b_stat).max();
-      this.random_std[0] = jStat(l_stat).stdev();
-      this.random_std[1] = jStat(e_stat).stdev();
-      this.random_std[2] = jStat(b_stat).stdev();
-    }
-    else{
-      this.random_mean.fill(0);
-      this.random_median.fill(0);
-      this.random_min.fill(0);
-      this.random_max.fill(0);
-      this.random_std.fill(0);
-    }
+    const edges = this.rs.treemapSelections.getEdges();
+    const clusters = this.rs.treemapSelections.getClusters();
+    const nodes = this.rs.treemapSelections.getNodes();
 
-    // treemapSelections = new TreemapSelections(treemapData, root);
-    treemapSelections = new TreemapSelectionsDevided(treemapData, root); //chan adding
-    // treemapEventListeners = new TreemapEventListeners(treemapData, treemapSelections);
-    treemapEventListeners = new TreemapEventListeners(treemapData, treemapSelections); //chan adding
-    edgeMeasurement = new EdgeMeasurement(treemapData, branch);
-    this.measurement = edgeMeasurement.calculateEdgeCrossingCount();
-
-    const edges = treemapSelections.getEdges();
-    const clusters = treemapSelections.getClusters();
-    const nodes = treemapSelections.getNodes();
-
-    clusters.on("mouseenter", (event, d) => {
+    clusters.on("mouseenter", (event:any, d:any) => {
       console.log("cluster mouseenter", event, d);
-      treemapEventListeners.clusterHighlightOn(event, d);
+      this.rs.treemapEventListeners.clusterHighlightOn(event, d);
       // treemapEventListeners.clusterNumberOn(event, d);
     })
-      .on("mouseleave", (event, d) => {
+      .on("mouseleave", (event:any, d:any) => {
         // console.log("cluster mouseleave", event, d);
-        treemapEventListeners.clusterHighlightOff(event, d);
+        this.rs.treemapEventListeners.clusterHighlightOff(event, d);
         // treemapEventListeners.clusterNumberOff(event, d);
       })
 
-    nodes.on("mouseover", (event, d) => {
+    nodes.on("mouseover", (event:any, d:any) => {
       console.log("node mouseover", event, d);
-      treemapEventListeners.restoreNodeAndTextSize(event, d);
-      treemapEventListeners.adjacentNodesHighlightOn(event, d);
-      treemapEventListeners.attachedEdgesHighlightOn(event, d);
-      treemapEventListeners.adjacentNodesTextHighlightOn(event, d);
+      this.rs.treemapEventListeners.restoreNodeAndTextSize(event, d);
+      this.rs.treemapEventListeners.adjacentNodesHighlightOn(event, d);
+      this.rs.treemapEventListeners.attachedEdgesHighlightOn(event, d);
+      this.rs.treemapEventListeners.adjacentNodesTextHighlightOn(event, d);
       tooltipOn(event, d);
     })
-      .on("mouseout", (event, d) => {
+      .on("mouseout", (event:any, d:any) => {
         // console.log("node mouseout", event, d);
         tooltipOff(event, d);
       })
-      .on("click", (event, d) => {
+      .on("click", (event:any, d:any) => {
         console.log("node click", event, d);
-        treemapEventListeners.magnifyViewBox(event, d);
+        this.rs.treemapEventListeners.magnifyViewBox(event, d);
         event.stopPropagation();
       });
 
@@ -320,7 +268,7 @@ export class TreemapComponent implements OnInit {
       .style('display', 'block');
 
     const tooltipOn = (event: any, d: any) => {
-      const clusterCount = treemapData.getClusterCount();
+      const clusterCount = this.rs.treemapData!.getClusterCount();
 
       toolTip
         .style('opacity', 1)
--- src/shared/modules/treemap-vis/datas/random-statistics.service.ts
+++ src/shared/modules/treemap-vis/datas/random-statistics.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { IBranchData } from 'src/shared/interfaces/ibranch-data';
+import { IBusData } from 'src/shared/interfaces/ibus-data';
+import { IStatisticData } from 'src/shared/interfaces/istatistic.data';
+import { EdgeMeasurement } from '../calculate edge crossing/edge-measurement';
+import { TreemapEventListeners } from '../event-listeners/treemap-event-listeners';
+import { TreemapSelections } from '../selections/treemap-selections';
+import { TreemapSelectionsDevided } from '../selections/treemap-selections-devided';
+import { global_Random_TreemapData } from './global-random-treemap-data';
+import { local_Random_TreemapData } from './local-random-treemap-data';
+import { seqeunce_TreemapData } from './seqeunce-treemap-data';
+import { TreemapData } from './treemap-data';
 
 declare var require: any //jStat 쓰기 위해 추가
 
@@ -6,6 +18,119 @@ declare var require: any //jStat 쓰기 위해 추가
   providedIn: 'root'
 })
 export class RandomStatisticsService {
+  togglenum = new Subject<number>();
+  togglenum$=new Subject<number>();
+  toggle = new Subject<string>();
+  iter = new Subject<string>();
+  iter$=new Subject<string>();
+  statisticdata:IStatisticData;
+  bus:IBusData[]|undefined;
+  branch:IBranchData[]|undefined;
+  details:any;
+  size:any;
+  nodeSize:any;
+  strokeWidth:any;
+  opacity:any;
+  // treemapSelections$=new Subject<TreemapSelectionsDevided>;
+  // treemapEventListeners$=new Subject<TreemapEventListeners>;
+  // edgeMeasurement$=new Subject<EdgeMeasurement>;
+  // measurement$=new Subject<number[]>;
+  treemapData:TreemapData | seqeunce_TreemapData | local_Random_TreemapData | global_Random_TreemapData|undefined;
+  treemapSelections:any;
+  treemapEventListeners:any;
+  edgeMeasurement:any;
+  measurement:number[];
+  root:any;
+  measurement$=new Subject<number[]>();
+  statisticdata$=new Subject<IStatisticData>();
 
-  constructor() { }
+  constructor() {
+    this.measurement=[];
+    let edgeMeasurement: EdgeMeasurement;
+    // let treemapSelections: TreemapSelections;
+    let treemapSelections: TreemapSelections | TreemapSelectionsDevided; //chan adding
+    let treemapEventListeners: TreemapEventListeners;
+    let random_count=1;
+    console.log('random_count: ', random_count);
+    var { jStat } = require('jstat');
+    let l_stat = new Array();//total length stat
+    let e_stat = new Array();//edge crossing stat
+    let b_stat = new Array();//total bending stat
+    this.statisticdata={
+      mean:[0,0,0],
+      median:[0,0,0],
+      min:[0,0,0],
+      max:[0,0,0],
+      std:[0,0,0]
+    };
+    this.iter.subscribe((d) => {
+      if (d == '') d = '1';
+      random_count = Number(d);
+      this.iter$.next(d);
+    })
+    this.togglenum.subscribe((d) => {
+      const nodeSize = 371.13274 * Math.pow(d, -0.42682);
+      this.togglenum$.next(d);
+    });
+    this.toggle.subscribe((d) => {
+      if (d == "Z_Layout")
+        this.treemapData = new TreemapData(this.bus!, this.branch!, this.details, this.size, this.nodeSize, this.strokeWidth, this.opacity);
+      else if (d == "Sequence")
+        this.treemapData = new seqeunce_TreemapData(this.bus!, this.branch!, this.details, this.size, this.nodeSize, this.strokeWidth, this.opacity);
+      else if (d == "Local_Random") {
+        for (let i = 0; i < random_count; i++) {
+          this.treemapData = new local_Random_TreemapData(this.bus!, this.branch!, this.details, this.size, this.nodeSize, this.strokeWidth, this.opacity);
+          this.treemapData.setZNodePosition();
+          edgeMeasurement = new EdgeMeasurement(this.treemapData, this.branch!);
+          l_stat.push(edgeMeasurement.calculateEdgeCrossingCount()[0]);
+          e_stat.push(edgeMeasurement.calculateEdgeCrossingCount()[1]);
+          b_stat.push(edgeMeasurement.calculateEdgeCrossingCount()[2]);
+        }
+      }
+      else if (d == "Global_Random") {
+        for (let i = 0; i < random_count; i++) {
+          this.treemapData = new global_Random_TreemapData(this.bus!, this.branch!, this.details, this.size, this.nodeSize, this.strokeWidth, this.opacity);
+          this.treemapData.setZNodePosition();
+          edgeMeasurement = new EdgeMeasurement(this.treemapData, this.branch!);
+          l_stat.push(edgeMeasurement.calculateEdgeCrossingCount()[0]);
+          e_stat.push(edgeMeasurement.calculateEdgeCrossingCount()[1]);
+          b_stat.push(edgeMeasurement.calculateEdgeCrossingCount()[2]);
+        }
+      }
+      this.treemapData!.setZNodePosition();
+      if (d == "Local_Random" || d == "Global_Random") {
+        this.statisticdata.mean[0] = jStat(l_stat).mean();
+        this.statisticdata.mean[1] = jStat(e_stat).mean();
+        this.statisticdata.mean[2] = jStat(b_stat).mean();
+        this.statisticdata.median[0] = jStat(l_stat).median();
+        this.statisticdata.median[1] = jStat(e_stat).median();
+        this.statisticdata.median[2] = jStat(b_stat).median();
+        this.statisticdata.min[0] = jStat(l_stat).min();
+        this.statisticdata.min[1] = jStat(e_stat).min();
+        this.statisticdata.min[2] = jStat(b_stat).min();
+        this.statisticdata.max[0] = jStat(l_stat).max();
+        this.statisticdata.max[1] = jStat(e_stat).max();
+        this.statisticdata.max[2] = jStat(b_stat).max();
+        this.statisticdata.std[0] = jStat(l_stat).stdev();
+        this.statisticdata.std[1] = jStat(e_stat).stdev();
+        this.statisticdata.std[2] = jStat(b_stat).stdev();
+      }
+      else {
+        this.statisticdata.mean.fill(0);
+        this.statisticdata.median.fill(0);
+        this.statisticdata.min.fill(0);
+        this.statisticdata.max.fill(0);
+        this.statisticdata.std.fill(0);
+      }
+      
+    // treemapSelections = new TreemapSelections(treemapData, root);
+    treemapSelections = new TreemapSelectionsDevided(this.treemapData!, this.root); //chan adding
+    // treemapEventListeners = new TreemapEventListeners(treemapData, treemapSelections);
+    treemapEventListeners = new TreemapEventListeners(this.treemapData!, treemapSelections); //chan adding
+    edgeMeasurement = new EdgeMeasurement(this.treemapData!, this.branch!);
+    this.measurement = edgeMeasurement.calculateEdgeCrossingCount();
+    this.measurement$.next(this.measurement);
+    this.statisticdata$.next(this.statisticdata);
+    });
+  }
 }
